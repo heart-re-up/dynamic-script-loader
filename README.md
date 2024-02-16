d2248a3c506bad4c0a263dc02fe94209ebcd6c0e
heart.re.up <chilly.soda@gmail.com>
2024-02-17 02:30:20 +0900

    동기 및 비동기 함수로 분리

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# dynamic-script-loader
+# @heartreup/script-loader
+
+# How to install.
+
+```shell
+# npm
+npm i @heartreup/script-loader
+
+# yarn
+yarn add @heartreup/script-loader
+
+# pnpm
+pnpm add @heartreup/script-loader
+```
+
+Load a script synchronously or asynchronously.
+
+# Usages.
+
+Use with callbacks.
+
+```ts
+import { loadScriptSync } from "@heartreup/script-loader";
+
+loadScriptSync(
+    'https://something.com/something/path/javascript-file.js',
+    (el: HTMLScriptElement) => console.log(el),
+    (ev: ErrorEvent) => console.error(ev),
+);
+```
+
+Use with a promise.
+
+```ts
+import { loadScript } from "@heartreup/script-loader";
+
+const load = async () => {
+    const el = await loadScript('https://something.com/something/path/javascript-file.js');
+}
+```
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,63 @@
-export function loadScript(url: string, async: boolean = true): Promise<HTMLScriptElement> {
-    return new Promise((resolve, reject) => {
-        let id = 0;
+/**
+ * url 을 hash 아이디로 변환합니다.
+ *
+ * @param url
+ */
+export function urlToHash(url: string): string {
+  let id = 0;
 
-        for (let i = 0; i < url.length; i++) {
-            id = (id << 5) - id + url.charCodeAt(i);
-            id |= 0;
-        }
+  for (let i = 0; i < url.length; i += 1) {
+    // eslint-disable-next-line no-bitwise
+    id = (id << 5) - id + url.charCodeAt(i);
+    // eslint-disable-next-line no-bitwise
+    id |= 0;
+  }
 
-        const exists = document.getElementById(String(id));
-        if (exists != null && exists instanceof HTMLScriptElement) {
-            resolve(exists as HTMLScriptElement)
-            return
-        }
+  return String(id);
+}
 
-        const el = document.createElement("script");
-        el.setAttribute('id', String(id))
-        el.setAttribute('src', url)
-        el.setAttribute('type', 'text/javascript')
-        el.setAttribute('async', String(async))
-        el.addEventListener('load', () => resolve(el))
-        el.addEventListener('error', (ev) => reject(ev))
-        document.body.append(el);
-    });
-};
+/**
+ * Load a script synchronously.
+ *
+ * @param url A url script file placed.
+ * @param async async attribute
+ * @param resolve load callback
+ * @param reject error callback
+ */
+export function loadScriptSync(
+  url: string,
+  async: boolean = true,
+  resolve?: (el: HTMLScriptElement) => void,
+  reject?: (ev: ErrorEvent) => void,
+) {
+  const id = urlToHash(url);
+  const foundScriptEl = document.getElementById(id);
+
+  // 이미 로드된 스크립트를 찾았다면 아무것도 안함
+  if (foundScriptEl != null && foundScriptEl instanceof HTMLScriptElement) {
+    resolve?.(foundScriptEl);
+    return;
+  }
+
+  // 아직 로드된 스크립트가 없음. 로드함.
+  const el = document.createElement<"script">("script");
+  el.setAttribute("id", String(id));
+  el.setAttribute("src", url);
+  el.setAttribute("type", "text/javascript");
+  el.setAttribute("async", String(async));
+  el.addEventListener("load", () => resolve?.(el));
+  el.addEventListener("error", (ev) => reject?.(ev));
+  document.body.append(el);
+}
+
+/**
+ * Load a script asynchronously.
+ *
+ * @param url A url script file placed.
+ * @param async async attribute
+ */
+export function loadScript(url: string): Promise<HTMLScriptElement> {
+  return new Promise((resolve, reject) => {
+    loadScriptSync(url, true, resolve, reject);
+  });
+}
